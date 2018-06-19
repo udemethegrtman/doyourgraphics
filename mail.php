b6aca5db71bb4438a409aacfba90a9fb8bd51d08
Udeme Samuel <udemesamuel256@gmail.com>
2018-06-19 01:59:30 -0700

    Updated comment on links to visit

--- mail.php
+++ mail.php
@@ -4,7 +4,9 @@ $telephone = $_POST['telephone'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "isaac.orija@gmail.com";
+$recipient = "info@iamlimo.github.io";
+// Issac try https://sourceforge.net/projects/phpmailer/
+// Or see the way i used it in Jobinpal Enterprises website https://github.com/JobinpalEnterprises/jobinpalwebsite
 $subject = "Contact Form";
 $mailheader ='From: Isaac Orija <isaac.orija@gmail.com>' . "\r\n"; header("Location: email-send.php");
 mail($recipient, $formcontent, $mailheader) or die("Error!");
